8dc91ecbdc76d651f12f2a4ffa82657151b7a842
Somesh <someshcr7b@gmail.com>
2022-07-28 16:58:24 +0530

    files stored in github

---
+++ src/App.js
@@ -0,0 +1,41 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.components";
+import { SearchBox } from "./components/search-box/search-box.components";
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/card-list/card-list.components.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Card } from "../card/card.component";
+
+import "./card-list.style.css";
+
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        // <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
+
+// export const CardList = (props) => (
+//   <div className="card-list">
+//     {props.monsters.map((monster) => (
+//       <CardList key={monster.id} monster={monster} />
+//     ))}
+//   </div>
+// );
